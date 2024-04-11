dc0589b44c91a826bc482e2fca67e74a44eef0ab
ParkHyeon89 <parkhyeon0809@gmail.com>
2024-04-11 19:21:01 +0900

    [설정] 박현 유니티 파일 업로드
    최적화 후 스크립트 재확인 했심다

---
+++ catTown/Assets/Hyeon/Script/Main_PRotate.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_PRotate : MonoBehaviour
+{
+        public float rotSpeed = 200f;
+
+        float mx = 0;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+        float mouse_X = Input.GetAxis("Mouse X");
+
+        // 회전 값 누적
+        mx += mouse_X * rotSpeed * Time.deltaTime;
+    
+        // 물제 회전 
+        transform.eulerAngles = new Vector3(0, mx, 0);
+
+
+    }
+}
