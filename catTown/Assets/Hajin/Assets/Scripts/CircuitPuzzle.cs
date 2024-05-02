8a03518db718a3cfccdc3840bc0677a1a9b1276d
Heo Nayeong <gjsk132@naver.com>
2024-05-02 13:44:27 +0900

    Revert "[설정] 아파트먼트 씬 업로드 (#62)"
    This reverts commit 86cf6ede5bcec3a5730f3a1aa6d2f820b2081dea.

--- catTown/Assets/Hajin/Assets/Scripts/CircuitPuzzle.cs
+++ catTown/Assets/Hajin/Assets/Scripts/CircuitPuzzle.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public class CircuitPuzzle : MonoBehaviour
 {
     public GameObject[] circuitObjects; // 회선 배열
+    public GameObject objectToDestroy; 
     public int[] correctSequence; // 정답 회선
 
     public Material defaultMaterial; // 기본 매터리얼
@@ -12,29 +13,19 @@ public class CircuitPuzzle : MonoBehaviour
     private List<int> clickedSequence = new List<int>(); // 클릭된 회선의 순서
     private Renderer[] circuitRenderers; // 회선 오브젝트 렌더러 배열
 
-    public bool is_DoorOpen = false;
-
-
-    private void Awake()
-    {
-        
-    }
-
-    private void Start() // 배열 초기화
+    private void Awake() // 배열 초기화
     {
         circuitRenderers = new Renderer[circuitObjects.Length];
         for (int i = 0; i < circuitObjects.Length; i++)
         {
             circuitRenderers[i] = circuitObjects[i].GetComponent<Renderer>();
             SetObjectMaterial(i, defaultMaterial); // 기본 매터리얼 설정
-            
         }
     }
 
     public void OnCircuitButtonClicked(int circuitIndex)
     {
         Debug.Log("Clicked Circuit: " + circuitIndex);
-
         clickedSequence.Add(circuitIndex); // 인덱스를 순서에 추가
 
         SetObjectMaterial(circuitIndex, clickedMaterial); // 클릭된 매터리얼
@@ -43,7 +34,7 @@ public class CircuitPuzzle : MonoBehaviour
         if (CheckCorrectSequence())
         {
             Debug.Log("Correct Sequence!"); 
-            is_DoorOpen = true;
+            Destroy(objectToDestroy); 
             ResetClickedSequence(); // 초기화
         }
         else if (clickedSequence.Count >= correctSequence.Length)
@@ -61,19 +52,15 @@ public class CircuitPuzzle : MonoBehaviour
         }
 
         // 순서확인
-        else
+        for (int i = 0; i < correctSequence.Length; i++)
         {
-            for (int i = 0; i < correctSequence.Length; i++)
+            if (clickedSequence[i] != correctSequence[i])
             {
-                if (clickedSequence[i] != correctSequence[i])
-                {
-                    return false; // 순서오답
-                }
+                return false; // 순서오답
             }
-
-            
-            return true; // 정답
         }
+
+        return true; // 정답
     }
 
     private void ResetClickedSequence()
