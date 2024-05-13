533826ea8e8deb80011a47d882210e5644efac13
Heo Nayeong <gjsk132@naver.com>
2024-05-14 04:01:05 +0900

    [설정] 허나영 파일 업데이트

--- catTown/Assets/Nayeong/Script/InteractionBasic.cs
+++ catTown/Assets/Nayeong/Script/InteractionBasic.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using System;
 
 public class InteractionBasic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float warningIndex = 0f;
+
+    public event EventHandler OnCoillsionWarningEvent;
+
     void Update()
     {
-        
+        warningIndex -= Time.deltaTime * 0.1f;
+        warningIndex = Mathf.Clamp(warningIndex, 0f, 100f);
     }
+
+    /// <summary>
+    /// 위험 지수를 올리는 오브젝트와의 충돌 시 일어나는 함수
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Warning Object"))
+        {
+            OnCoillsionWarningEvent?.Invoke(this, EventArgs.Empty);
+
+            warningIndex += 0.2f;
+            warningIndex = Mathf.Clamp(warningIndex, 0f, 100f);
+
+            Rigidbody rd = collision.gameObject.GetComponent<Rigidbody>();
+            if (rd != null)
+            {
+                Vector3 pushDirection = (collision.transform.position - transform.position).normalized;
+                rd.AddForce(pushDirection * 1f, ForceMode.Impulse);
+            }
+        }
+    }
+
 }
