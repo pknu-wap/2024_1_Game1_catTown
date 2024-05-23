6dc7e98296bc7d06f15d2318293dadd0f08d2c43
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-05-23 20:53:56 +0900

    [설정] 이전 파일 복구 (3)
    한글 깨짐으로 인한 파일 충돌 복구
    Player.cs

--- catTown/Assets/Junyoung/Scripts/Junyoung/Player.cs
+++ catTown/Assets/Junyoung/Scripts/Junyoung/Player.cs
@@ -10,10 +10,11 @@ public class Player : MonoBehaviour
     [SerializeField] float mouseX;
     [SerializeField] float rotateSpeed;
     [SerializeField] Vector3 direction;
-    public bool dead { get; protected set; }
+    public bool dead { get; protected set; } // 사망 상태
 
     protected virtual void OnEnable()
     {
+        // 사망하지 않은 상태로 시작
         dead = false;
         
     }
@@ -38,6 +39,7 @@ public class Player : MonoBehaviour
 
         direction.Normalize();
 
+        // TransformDirection : 자기가 바라보고 있는 방향으로 이동하는 함수입니다.
         transform.position += transform.TransformDirection(direction) * speed * Time.deltaTime;
     }
 
