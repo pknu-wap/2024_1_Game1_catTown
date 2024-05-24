4eee0da01604294f2bde1173e20327c3fce75cae
Heo Nayeong <gjsk132@naver.com>
2024-05-24 21:28:11 +0900

    [오브젝트] 아이템 상호작용 스크립트 수정

--- catTown/Assets/Nayeong/Script/InteractionItem.cs
+++ catTown/Assets/Nayeong/Script/InteractionItem.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class InteractionItem : MonoBehaviour
 {
     GameObject player;
-    GameObject item;
+
+    private int healValue;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("HpItem"))
         {
-            Debug.Log("플레이어 체력 아이템 충돌");
             player = GameObject.Find("Player");
+
+            healValue = other.GetComponent<ItemStatus>().healValue;
+            if (healValue > 0)
+            {
+                Debug.Log(healValue+"만큼 회복");
+            }
+            else
+            {
+                Debug.Log(healValue+"만큼 감소");
+            }
+
             player.GetComponent<Main_PMove>().hp += other.GetComponent<ItemStatus>().healValue;
             
             other.gameObject.SetActive(false);
