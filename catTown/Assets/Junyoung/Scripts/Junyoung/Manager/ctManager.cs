3a7ba07b4830d8aeae079cde6ce95c6a67b8b52f
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-05-30 22:12:54 +0900

    Jody bool wakeup = false 추가

---
+++ catTown/Assets/Junyoung/Scripts/Junyoung/Manager/ctManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ctManager : MonoBehaviour
+{
+    public Main_PMove player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
