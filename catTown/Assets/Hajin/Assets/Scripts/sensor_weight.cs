17ffed242c94ccf4168aa6dcbe1d84ee576f28f2
westging <seohajin0625@gmail.com>
2024-06-06 07:38:33 +0900

    [맵] 공사장 씬 상자 상호작용 추가와 크레인 추가
    공사장 씬 상자 상호작용 추가와 크레인 추가

--- catTown/Assets/Hajin/Assets/Scripts/sensor_weight.cs
+++ catTown/Assets/Hajin/Assets/Scripts/sensor_weight.cs
@@ -4,14 +4,25 @@ public class WeightDetection : MonoBehaviour
 {
     public float weightThreshold = 10f; 
     public GameObject objectToDestroy; 
+    public float weight;
     Animator anim;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        anim = objectToDestroy.GetComponent<Animator>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>(); 
         if (rb != null)
         {
-            float weight = rb.mass; 
+            weight = rb.mass; 
+            
             if (weight >= weightThreshold)
             {
                 anim.SetTrigger("Open");
