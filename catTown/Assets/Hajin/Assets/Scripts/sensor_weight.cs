cc33c44d8c08b06bb08dbe41a1b4398b529ffc5f
westging <109637733+westging@users.noreply.github.com>
2024-06-07 02:16:34 +0900

    [맵] 크레인, 무게 센서, 애니메이션 수정 (#118)
    * [맵] 공사장 씬 상자 상호작용 추가와 크레인 추가
    
    공사장 씬 상자 상호작용 추가와 크레인 추가
    
    * [맵] 크레인, 무게 센서, 애니메이션 수정
    
    공사장 크레인, 무게 센서 오류 수정 및 아파트 애니메이션 수정
    
    ---------
    
    Co-authored-by: Heo Nayeong <gjsk132@naver.com>

--- catTown/Assets/Hajin/Assets/Scripts/sensor_weight.cs
+++ catTown/Assets/Hajin/Assets/Scripts/sensor_weight.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class WeightDetection : MonoBehaviour
 {
+<<<<<<< HEAD
+    public float weightThreshold = 10f;
+    public GameObject objectToDestroy;
+    public float totalWeight; // 전체 무게 저장할 변수 추가
+    
+    Animator anim;
+
+=======
     public float weightThreshold = 10f; 
     public GameObject objectToDestroy; 
     public float weight;
@@ -11,6 +19,7 @@ public class WeightDetection : MonoBehaviour
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
     /// </summary>
+>>>>>>> develop
     void Start()
     {
         anim = objectToDestroy.GetComponent<Animator>();
@@ -18,17 +27,29 @@ public class WeightDetection : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>(); 
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
         if (rb != null)
         {
+<<<<<<< HEAD
+            totalWeight += rb.mass; // 전체 무게에 충돌한 오브젝트 무게 추가
+            if (totalWeight >= weightThreshold)
+=======
             weight = rb.mass; 
             
             if (weight >= weightThreshold)
+>>>>>>> develop
             {
                 anim.SetTrigger("Open");
             }
         }
     }
 
-
+    private void OnCollisionExit(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            totalWeight -= rb.mass; // 충돌 해제 시 전체 무게에서 제거
+        }
+    }
 }
